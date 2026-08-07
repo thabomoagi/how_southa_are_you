a06aabb7793bf16abb7b27cf9e5f4a8e356df97c
Thabo Moagi <tboydaflame@gmail.com>
2026-08-08 00:30:20 +0200

    Fix main class configuration for Docker build

---
+++ src/main/java/com/thabo/howsouthaareyou/HowSaAreYouApplication.java
@@ -0,0 +1,12 @@
+package com.thabo.howsouthaareyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowSaAreYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowSaAreYouApplication.class, args);
+    }
+}
